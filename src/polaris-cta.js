13e1f48ee747f1bd57612526e4dbb29cd6029f4f
trip <daxton416@gmail.com>
2023-10-31 22:42:47 -0400

    Improved many things and fixed many bugs

--- src/polaris-cta.js
+++ src/polaris-cta.js
@@ -18,10 +18,12 @@ class PolarisCta extends LitElement {
 
       outlined: {
         type: Boolean,
+        reflect: true
       },
 
       filled: {
         type: Boolean,
+        reflect: true
       },
     }
   }
@@ -34,44 +36,41 @@ class PolarisCta extends LitElement {
       --polaris-cta-psu-gray:#E4E5E7;
       --polaris-cta-psu-darkgray: #4f627C;
       --polaris-cta-psu-blue: #1E407C;
-      --polaris-cta-psu-darkblue: #001E44;
-      --polaris-cta-psu-lightblue:#96BEE6;
+      --polaris-cta-psu-darkblue: #011E44;
+      --polaris-cta-psu-lightblue:#A5E7fE;
       --polaris-cta-psu-black: #000000;
     }
 
     a {
       text-transform: uppercase;
       text-decoration: none;
+      font-size: 16px;
       font-style: italic;
       font-weight: 700;
       letter-spacing: .025rem;
       margin-left: 32px;
       border-radius: 4px;
-      display: inline-flex;
-    }
-
-    :host([type="primary"][outlined]) a {
-      border: 2px solid var(--polaris-cta-psu-blue);
-      background-color: var(--polaris-cta-psu-white);
-      color: var(--polaris-cta-psu-blue);
       padding: 12px 32px;
       transition: 0.1s;
     }
 
-    :host([type="primary"][outlined]) a:focus,
-    :host([type="primary"][outlined]) a:hover {
+    :host([type="primary"]) a {
+      border: 2px solid var(--polaris-cta-psu-blue);
+      background-color: var(--polaris-cta-psu-white);
+      color: var(--polaris-cta-psu-blue);
+    }
+
+    :host([type="primary"]) a:focus,
+    :host([type="primary"]) a:hover {
       border: 2px solid var(--polaris-cta-psu-blue);
       background-color: var(--polaris-cta-psu-blue);
       color: var(--polaris-cta-psu-white);
-      padding: 12px 32px;
     }
 
     :host([type="primary"][filled]) a {
       border: 2px solid var(--polaris-cta-psu-blue);
       background-color: var(--polaris-cta-psu-blue);
       color: var(--polaris-cta-psu-white);
-      padding: 12px 32px;
-      transition: 0.1s;
     }
 
     :host([type="primary"][filled]) a:focus,
@@ -79,22 +78,25 @@ class PolarisCta extends LitElement {
       border: 2px solid var(--polaris-cta-psu-blue);
       background-color: var(--polaris-cta-psu-white);
       color: var(--polaris-cta-psu-blue);
-      padding: 12px 32px;
     }
 
     :host([type="tinted"]) a {
       border: 2px solid var(--polaris-cta-psu-white);
       background-color: var(--polaris-cta-psu-darkgray);
       color: var(--polaris-cta-psu-lightblue);
-      padding: 12px 32px;
+    }
+
+    :host([type="tinted"]) a:focus,
+    :host([type="tinted"]) a:hover {
+      border: 2px solid var(--polaris-cta-psu-darkgray);
+      background-color: var(--polaris-cta-psu-white);
+      color: var(--polaris-cta-psu-darkgray);
     }
 
     :host([type="light"][outlined]) a {
       border: 2px solid var(--polaris-cta-psu-white);
       background-color: var(--polaris-cta-psu-darkblue);
       color: var(--polaris-cta-psu-lightblue);
-      padding: 12px 32px;
-      transition: 0.1s;
     }
 
     :host([type="light"][outlined]) a:focus,
@@ -102,15 +104,12 @@ class PolarisCta extends LitElement {
       border: 2px solid var(--polaris-cta-psu-lightblue);
       background-color: var(--polaris-cta-psu-lightblue);
       color: var(--polaris-cta-psu-darkblue);
-      padding: 12px 32px;
     }
 
     :host([type="light"][filled]) a {
       border: 2px solid var(--polaris-cta-psu-lightblue);
       background-color: var(--polaris-cta-psu-lightblue);
       color: var(--polaris-cta-psu-darkblue);
-      padding: 12px 32px;
-      transition: 0.1s;
     }
 
     :host([type="light"][filled]) a:focus,
@@ -118,15 +117,12 @@ class PolarisCta extends LitElement {
       border: 2px solid var(--polaris-cta-psu-white);
       background-color: var(--polaris-cta-psu-darkblue);
       color: var(--polaris-cta-psu-lightblue);
-      padding: 12px 32px;
     }
   `;
 
   constructor() {
     super();
-    this.text = 'Subscribe';
-    this.link = 'https://psu.edu';
-    this.type = 'tinted';
+    this.type = 'primary';
   }
 
   render() {
